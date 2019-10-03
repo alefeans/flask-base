b2d37191b66bd244beb84b921509c9291521d5a5
Alefe Silva <alefeans@localhost.localdomain>
2019-10-03 14:30:04 -0300

    setup required fields

--- app/v1/resources/users/serializers.py
+++ app/v1/resources/users/serializers.py
@@ -4,11 +4,11 @@ from flask_restplus import fields
 
 user = api.model('User', {
     'id': fields.String(readonly=True, description='User ID', attribute='_id.$oid'),
-    'username': fields.String(description='The Username'),
-    'email': fields.String(description='User Email')
+    'username': fields.String(required=True, description='The Username'),
+    'email': fields.String(required=True, description='User Email')
 })
 
 
 create_user = api.inherit('User Creation', user, {
-    'password': fields.String(description='User Password'),
+    'password': fields.String(required=True, description='User Password'),
 })
